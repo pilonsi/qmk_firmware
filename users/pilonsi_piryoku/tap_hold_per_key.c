1625db9a196a3824b94b9f2b94f42c4ce43c6ad1
Jorge Fabregat <jfabregat@protonmail.com>
2021-10-21 11:40:59 +0200

    Added my WIP Miryoku derivative

---
+++ users/pilonsi_piryoku/tap_hold_per_key.c
@@ -0,0 +1,25 @@
+// Copyright 2021, Pilonsi
+// My personal 36-key layout based off a modified version of the wonderful Miryoku layout, by Manna Harbour
+// https://github.com/manna-harbour/miryoku
+
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case LGUI_T(KC_A):
+    case LGUI_T(KC_QUOT):
+      return TAPPING_TERM + 50;
+    case LALT_T(KC_S):
+    case LALT_T(KC_L):
+      return TAPPING_TERM + 40;
+    case LCTL_T(KC_D):
+    case LCTL_T(KC_K):
+      return TAPPING_TERM;
+    case LSFT_T(KC_F):
+    case LSFT_T(KC_J):
+      return TAPPING_TERM - 20;
+    default:
+      return TAPPING_TERM;
+  }
+}
+
