6cb2bbd036954dd75fac2a7152cc884cf2eba00e
Jorge Fabregat <jfabregat@protonmail.com>
2022-02-03 10:00:51 +0100

    Updated layout and tap-hold settings

--- users/pilonsi_piryoku/pilonsi_piryoku.c
+++ users/pilonsi_piryoku/pilonsi_piryoku.c
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,
     LGUI_T(KC_A),      LALT_T(KC_S),      LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G,              KC_H,              LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      LGUI_T(KC_QUOT),
     KC_Z,              ALGR_T(KC_X),      KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           ALGR_T(KC_DOT),    KC_SLSH,
-    U_NP,              U_NP,              LT(MEDIA, KC_ESC), LT(NAV, KC_SPC),   LT(SHORT, KC_TAB), LT(CAT, KC_ENT),   LT(NUM, KC_BSPC),  LT(FUN, KC_DEL),   U_NP,              U_NP
+    U_NP,              U_NP,              LT(MESH, KC_ESC),  LT(NAV, KC_SPC),   LT(CAT, KC_TAB),   LT(SYM, KC_ENT),   LT(NUM, KC_BSPC),  LT(FUN, KC_DEL),   U_NP,              U_NP
   ),
   [NAV] = LAYOUT_piryoku(
     RESET,   U_NA,    U_NA,    U_NA,    U_NA,    U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,
@@ -24,16 +24,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     U_NP,    U_NP,    U_NA,    U_NA,    U_NA,    KC_ENT,  KC_BSPC, KC_DEL,  U_NP,    U_NP
   ),
   [CAT] = LAYOUT_piryoku(
-    U_NU,    U_NU,    U_NU,    U_NU,    U_NU,    U_NA,    U_NA,    U_NA,    U_NA,    RESET,
-    U_NU,    U_NU,    U_NU,    U_NU,    U_NU,    U_NA,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
-    U_NU,    U_NU,    U_NU,    U_NU,    U_NU,    U_NA,    U_NA,    U_NA,    KC_ALGR, U_NA,
-    U_NP,    U_NP,    KC_LPRN, KC_RPRN, KC_UNDS, U_NA,    U_NA,    U_NA,    U_NP,    U_NP
+    RESET,   U_NA,    U_NA,    U_NA,    U_NA,    CA_LCLL, CA_PVOL, CA_OEXL, CA_OINT, CA_RCLL,
+    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, U_NA,    CA_ENYA, CA_CETR, CA_ACTA, CA_ACOB, CA_DIER,
+    U_NA,    KC_ALGR, U_NA,    U_NA,    U_NA,    U_NU,    CA_LLAE, CA_LLOE, CA_ACCF, U_NU,
+    U_NP,    U_NP,    U_NA,    U_NA,    U_NA,    KC_ENT,  KC_BSPC, KC_DEL,  U_NP,    U_NP
   ),
-  [MEDIA] = LAYOUT_piryoku(
-    RESET,   U_NA,    U_NA,    U_NA,    U_NA,    RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI,
-    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, U_NA,    KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, U_NU,
-    U_NA,    KC_ALGR, U_NA,    U_NA,    U_NA,    U_NU,    U_NU,    U_NU,    U_NU,    U_NU,
-    U_NP,    U_NP,    U_NA,    U_NA,    U_NA,    KC_MSTP, KC_MPLY, KC_MUTE, U_NP,    U_NP
+  [MESH] = LAYOUT_piryoku(
+    RESET,             KC_F5,             KC_F3,             KC_F1,             A(KC_G),           RGB_TOG,           RGB_MOD,           RGB_HUI,           RGB_SAI,           RGB_VAI,
+    LGUI_T(U_UND),     LALT_T(U_CUT),     LCTL_T(U_CPY),     LSFT(U_PST),       U_RDO,             KC_MPRV,           KC_VOLD,           KC_VOLU,           KC_MNXT,           U_NU,
+    U_NU,              U_NA,              KC_BSPC,           KC_DEL,            U_NU,              U_NU,              U_NU,              U_NU,              U_NU,              U_NU,
+    U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_MUTE,           KC_MPLY,           KC_MSTP,           U_NP,              U_NP
   ),
   [NUM] = LAYOUT_piryoku(
     KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
@@ -41,11 +41,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS, U_NA,    U_NA,    U_NA,    KC_ALGR, U_NA,
     U_NP,    U_NP,    KC_DOT,  KC_0,    KC_MINS, U_NA,    U_NA,    U_NA,    U_NP,    U_NP
   ),
-  [SHORT] = LAYOUT_piryoku(
-    U_NU,    U_NU,    U_NU,    U_NU,    U_NU,    U_NA,    U_NA,    U_NA,    U_NA,    RESET,
-    U_NU,    KC_F1,   KC_F3,   KC_DEL,  U_NU,    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,
-    U_UND,   U_CUT,   U_CPY,   U_PST,   U_RDO,   U_NA,    U_NA,    U_NA,    U_NA,    U_NA,
-    U_NP,    U_NP,    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NP,    U_NP
+  [SYM] = LAYOUT_piryoku(
+    KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
+    KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS, U_NA,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE, U_NA,    U_NA,    U_NA,    KC_ALGR, U_NA,
+    U_NP,    U_NP,    KC_LPRN, KC_RPRN, KC_UNDS, U_NA,    U_NA,    U_NA,    U_NP,    U_NP
   ),
   [FUN] = LAYOUT_piryoku(
     KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
