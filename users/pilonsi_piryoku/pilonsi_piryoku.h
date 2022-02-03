4c973b8d7b3fb41946a4dfe80533578bdef7f0b9
Jorge Fabregat <jfabregat@protonmail.com>
2022-02-03 10:00:51 +0100

    Updated layout and tap-hold settings

--- users/pilonsi_piryoku/pilonsi_piryoku.h
+++ users/pilonsi_piryoku/pilonsi_piryoku.h
@@ -8,25 +8,29 @@
 
 #include QMK_KEYBOARD_H
 
+enum layers { BASE, MESH, NAV, CAT, SYM, NUM, FUN };
+
+/* Keycodes */
+
+// Miryoku unused keycodes
 #define U_NP KC_NO // key is not present
 #define U_NA KC_NO // present but not available for use
 #define U_NU KC_NO // available but not used
 
-enum layers { BASE, NAV, CAT, MEDIA, NUM, SHORT, FUN };
-
-#if defined PIRYOKU_CLIPBOARD_FUN
+// Miryoku clipboard options (Swapped default)
+#if defined PILONSI_CLIPBOARD_FUN
 #define U_RDO KC_AGIN
 #define U_PST KC_PSTE
 #define U_CPY KC_COPY
 #define U_CUT KC_CUT
 #define U_UND KC_UNDO
-#elif defined PIRYOKU_CLIPBOARD_MAC
+#elif defined PILONSI_CLIPBOARD_MAC
 #define U_RDO SCMD(KC_Z)
 #define U_PST LCMD(KC_V)
 #define U_CPY LCMD(KC_C)
 #define U_CUT LCMD(KC_X)
 #define U_UND LCMD(KC_Z)
-#elif defined PIRYOKU_CLIPBOARD_CUA
+#elif defined PILONSI_CLIPBOARD_CUA
 #define U_RDO KC_AGIN
 #define U_PST S(KC_INS)
 #define U_CPY C(KC_INS)
@@ -39,3 +43,18 @@ enum layers { BASE, NAV, CAT, MEDIA, NUM, SHORT, FUN };
 #define U_CUT C(KC_X)
 #define U_UND C(KC_Z)
 #endif
+
+// Catalan keycodes (Require US-Intl. layout)
+#define CA_LCLL ALGR(KC_LBRC) // « (Cometes llatines obertes)
+#define CA_ENYA ALGR(KC_N)    // Ñ (Enya)
+#define CA_OEXL ALGR(KC_1)    // ¡ (Exclamació oberta)
+#define CA_OINT ALGR(KC_SLSH) // ¿ (Interrogació oberta)
+#define CA_RCLL ALGR(KC_RBRC) // » (Cometes llatines tancades)
+#define CA_PVOL KC_NO         // · (Punt volat) [Not present on US-Intl. layout, looking for the less painful workaround)
+#define CA_CETR ALGR(KC_COMM) // Ç (Ce trencada)
+#define CA_ACTA KC_QUOT       // ´ (Accent tancat)
+#define CA_ACOB KC_GRV        // ` (Accent obert)
+#define CA_DIER S(KC_QUOT)    // ¨ (Dièresi)
+#define CA_LLAE KC_NO         // Æ (Lligadura d'a i e) [Not present on US-Intl. layout, looking for the less painful workaround)
+#define CA_LLOE KC_NO         // Œ (Lligadura d'o i e) [Not present on US-Intl. layout, looking for the less painful workaround)
+#define CA_ACCF S(KC_6)       // ^ (Accent circumflex)
