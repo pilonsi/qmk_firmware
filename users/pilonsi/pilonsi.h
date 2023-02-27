7cfc641ccafb38794fc3c3dace75fc945bba6981
Jorge Fabregat López <jfabregat@protonmail.com>
2023-02-27 10:54:36 +0100

    Updated to revision 12

---
+++ users/pilonsi/pilonsi.h
@@ -0,0 +1,47 @@
+// Copyright 2019 Manna Harbour
+// Copyright 2023 Pilonsi
+// My personal 36-key layout based off a modified version of the wonderful Miryoku layout, by Manna Harbour
+// https://github.com/manna-harbour/miryoku
+
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include QMK_KEYBOARD_H
+
+enum layers { BASE, MEDIA, NAV, EXT, SYM, NUM, FUN };
+
+/* Keycodes */
+
+// Miryoku unused keycodes
+#define U_NP KC_NO // key is not present
+#define U_NA KC_NO // present but not available for use
+#define U_NU KC_NO // available but not used
+
+// Clipboard setting
+#define U_CBM KC_MAIL
+
+// Clipboard options
+#if defined PILONSI_FIXED_CLIPBOARD_MAC
+#define U_RDO SCMD(KC_Z)
+#define U_PST LCMD(KC_V)
+#define U_CPY LCMD(KC_C)
+#define U_CUT LCMD(KC_X)
+#define U_UND LCMD(KC_Z)
+#elif defined PILONSI_FIXED_CLIPBOARD_WIN
+#define U_RDO C(KC_Y)
+#define U_PST C(KC_V)
+#define U_CPY C(KC_C)
+#define U_CUT C(KC_X)
+#define U_UND C(KC_Z)
+#else
+#define U_RDO KC_AGIN
+#define U_PST KC_PSTE
+#define U_CPY KC_COPY
+#define U_CUT KC_CUT
+#define U_UND KC_UNDO
+#endif
+
+/* Clipboard modes */
+#define PILONSI_CB_MODE_MAC 0x00
+#define PILONSI_CB_MODE_WIN 0x01
