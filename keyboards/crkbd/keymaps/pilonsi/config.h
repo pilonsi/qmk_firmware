39a7ccfd331df30399eb56d72e840d3c1f79fbea
Jorge Fabregat López <jfabregat@protonmail.com>
2023-02-27 10:54:36 +0100

    Updated to revision 12

--- keyboards/crkbd/keymaps/pilonsi/config.h
+++ keyboards/crkbd/keymaps/pilonsi/config.h
@@ -3,7 +3,8 @@ This is the c configuration file for the keymap
 
 Copyright 2012 Jun Wako <wakojun@gmail.com>
 Copyright 2015 Jack Humbert
-Copyright 2019 Manna Harbour, Pilonsi
+Copyright 2019 Manna Harbour
+Copyright 2023 Pilonsi
 
 My personal 36-key layout based off a modified version of the wonderful
 Miryoku layout, by Manna Harbour.
@@ -73,7 +74,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
 
-#define LAYOUT_piryoku(\
+#define LAYOUT_pilonsi(\
 K00,   K01,   K02,   K03,   K04,          K05,   K06,   K07,   K08,   K09,\
 K10,   K11,   K12,   K13,   K14,          K15,   K16,   K17,   K18,   K19,\
 K20,   K21,   K22,   K23,   K24,          K25,   K26,   K27,   K28,   K29,\
--- layouts/community/ergodox/pilonsi/config.h
+++ layouts/community/ergodox/pilonsi/config.h
@@ -1,4 +1,5 @@
-// Copyright 2019 Manna Harbour, Pilonsi
+// Copyright 2019 Manna Harbour
+// Copyright 2023 Pilonsi
 // My personal 36-key layout based off a modified version of the wonderful Miryoku layout, by Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
@@ -8,7 +9,7 @@
 
 #define XXX KC_NO
 
-#define LAYOUT_piryoku(\
+#define LAYOUT_pilonsi(\
      K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09,\
      K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19,\
      K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29,\
