687a26c1d0235cfd1c29db909b4b658afa6cab8d
Jorge Fabregat López <jfabregat@protonmail.com>
2023-02-28 13:13:23 +0100

    Adopted Luna for my Corne and reorganized files

--- users/pilonsi/process_record_user_common.c
+++ users/pilonsi/process_record_user_common.c
@@ -4,10 +4,6 @@
 
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-static uint8_t pilonsi_clipboard_mode = PILONSI_CB_MODE_MAC;
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
     case U_CBM:
       if (record->event.pressed) {
         unregister_code16(U_CBM);
@@ -97,7 +93,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
       } 
       break;
-
-  }
-  return true;
-}
