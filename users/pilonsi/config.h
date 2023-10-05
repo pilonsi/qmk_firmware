33be47fa209813ed99669479a52aa31b8249a022
Jorge Fabregat López <jfabregat@protonmail.com>
2023-10-05 23:36:07 +0200

    Updated keymap to build with latest QMK

--- users/pilonsi/config.h
+++ users/pilonsi/config.h
@@ -12,9 +12,6 @@
 #define TAPPING_TERM 170
 #define TAPPING_TERM_PER_KEY
 
-// Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
-
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
 
