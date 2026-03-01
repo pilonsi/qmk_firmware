d882d2b7c9c24231f5be3cce7cfd247f64120262
Jorge Fabregat López <jfabregat@protonmail.com>
2026-03-01 12:23:58 +0100

    14.0 - QMK 0.31.12

--- users/pilonsi/README.md
+++ users/pilonsi/README.md
@@ -1,7 +1,7 @@
 # My QMK Layout
 This is my personal 36-key layout, it is a modified version of the wonderful [Miryoku](https://github.com/manna-harbour/miryoku/) layout by Manna Harbour.
 
-![Layout Reference](https://i.imgur.com/CT1laIk.png)
+![Layout Reference](https://i.imgur.com/41igRwu.png)
 
 The layout as-is here is intended to be used with the following setup:
 
--- users/pilonsi/pilonsi.h
+++ users/pilonsi/pilonsi.h
@@ -1,47 +1,52 @@
 // Copyright 2019 Manna Harbour
-// Copyright 2023 Pilonsi
-// My personal 36-key layout based off a modified version of the wonderful Miryoku layout, by Manna Harbour
+// Copyright 2023-2026 Pilonsi
+// My personal 36-key layout based off a modified version of the wonderful
+// Miryoku layout, by Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 2 of the License, or (at your option)
+// any later version. This program is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details. You should have received a copy of the GNU
+// General Public License along with this program. If not, see
+// <http://www.gnu.org/licenses/>.
 
 #pragma once
 
 #include QMK_KEYBOARD_H
 
-#define PILONSI_VERSION_STRING "Pilonsi 12.2"
+#define PILONSI_VERSION_STRING "14.0"
 
-enum layers { BASE, MEDIA, NAV, EXT, SYM, NUM, FUN };
+enum layers { BASE, MEDIA, NAV, CAT, SYM, NUM, FUN };
 
-/* Keycodes */
-enum my_keycodes { U_OLED, U_CBM, U_VER };
+enum dead_key_states { DK_NONE, DK_ACUTE, DK_GRAVE, DK_DIERESIS };
 
-// Miryoku unused keycodes
 #define U_NP KC_NO // key is not present
 #define U_NA KC_NO // present but not available for use
 #define U_NU KC_NO // available but not used
 
-// Clipboard options
-#if defined PILONSI_FIXED_CLIPBOARD_MAC
-#define U_RDO SCMD(KC_Z)
-#define U_PST LCMD(KC_V)
-#define U_CPY LCMD(KC_C)
-#define U_CUT LCMD(KC_X)
-#define U_UND LCMD(KC_Z)
-#elif defined PILONSI_FIXED_CLIPBOARD_WIN
-#define U_RDO C(KC_Y)
-#define U_PST C(KC_V)
-#define U_CPY C(KC_C)
-#define U_CUT C(KC_X)
-#define U_UND C(KC_Z)
-#else
-#define U_RDO KC_AGIN
-#define U_PST KC_PSTE
-#define U_CPY KC_COPY
-#define U_CUT KC_CUT
-#define U_UND KC_UNDO
-#endif
+enum my_keycodes {
+  // Custom settings
+  P_MODE = SAFE_RANGE, // Cycle OS mode
+  P_SAVE,	// Save OS mode
+  P_OLED, // Toggle OLED
+  P_VER,  // Show version string
 
-/* Clipboard modes */
-#define PILONSI_CB_MODE_MAC 0x00
-#define PILONSI_CB_MODE_WIN 0x01
+  // OS dependent shortcuts
+  P_RDO,
+  P_PST,
+  P_CPY,
+  P_CUT,
+  P_UND,
+
+  // Catalan characters
+  CA_CC,  // Ç
+  CA_AT,  // ´
+  CA_AO,  // `
+  CA_DI,  // ¨
+  CA_PV,  // ·
+  CA_NY,  // Ñ
+};
